2d894b616b1f3fe124cc6389a07d9ab1557e59a2
Milos Popovic <milos_popovic@outlook.com>
2020-07-08 03:58:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   array = ["cat","dog", "mouse", "hamster"]
-  rarray.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
